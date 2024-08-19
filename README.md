3229b9cab32849b0baac154befd841085993f3ac
Slowsby <quentin.peckels@gmail.com>
2024-08-19 13:34:23 +0200

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# GAPE_DOWNLOADER
+
+Node.js script to batch download GAPE Imagery
+
+## Getting Started
+
+Follow these steps to set up and run the script:
+
+```bash
+  # CLONE THE REPO
+  git clone 
+  cd GAPE_DOWNLOADER
+
+  # INSTALL DEPENDENCIES
+  npm i
+
+  # CREATE A .env FILE IN ROOT FOLDER
+  #/.env
+  API_KEY=YOURAPIKEY
+
+  # RUN THE SCRIPT
+  npm run start -- EXPEDITION_NUMBER
+```
+EXPEDITION_NUMBER should follow GAPE'S [Database Mission ID](https://eol.jsc.nasa.gov/FAQ/default.htm#cameraMetadata_Mission)
---
+++ index.js
@@ -0,0 +1,81 @@
+import fs from 'fs';
+import path from 'path'
+import fetch from 'node-fetch';
+import axios from 'axios'
+import dotenv from 'dotenv';
+dotenv.config();
+const API_KEY = process.env.API_KEY;
+//Correct format to download images "https://eol.jsc.nasa.gov/DatabaseImages/images.directory/images.filename"
+const baseForm = "https://eol.jsc.nasa.gov/DatabaseImages/";
+
+
+let args = process.argv.slice(2)
+const expedition = String(args)
+// Change expidition naming following the "Database Mission ID" : https://eol.jsc.nasa.gov/FAQ/default.htm#cameraMetadata_Mission
+//const expedition = "ISS001"
+// API DOCS : https://eol.jsc.nasa.gov/SearchPhotos/PhotosDatabaseAPI/#dtaf
+const url = `https://eol.jsc.nasa.gov/SearchPhotos/PhotosDatabaseAPI/PhotosDatabaseAPI.pl?query=images|mission|eq|${expedition}|images|directory|not%20like|*small*|images|directory|not%20like|*lowres*|images|directory|not%20like|*registered*|images|directory|not%20like|*ReefBase*|images|directory|not%20like|*custom*|images|directory|not%20like|*EO*&return=images|directory|images|filename&key=${API_KEY}`;
+//Consecutive images to download
+const batch = 20;
+
+const fetchImages = async () => {
+    let firstIndex = 0;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(`Length: ${data.length}`);
+    while (firstIndex < data.length) {
+        const lastIndex = Math.min(firstIndex + batch, data.length);
+        const toDownload = data.slice(firstIndex, lastIndex);
+        
+        // Waits for the completion of the promise before starting new batch
+        await Promise.all(toDownload.map(async (el) => {
+            await downloadImage(el);
+        }));
+
+        // Update the index for the next batch
+        firstIndex = lastIndex;
+    }
+}
+
+const downloadImage = async (el) => {
+    return new Promise(async (resolve, reject) => {
+    try {
+        const filePath = path.resolve("GAPE", expedition, el['images.filename']);
+        const tmpFilePath = filePath + '.tmp';
+
+        // Check if the file already exists
+        if (fs.existsSync(filePath)) {
+            console.log(`File already exists: ${filePath}`);
+            resolve()
+            return;
+        }
+        fs.mkdirSync(path.dirname(filePath), { recursive: true });
+
+        const response = await axios({
+            url: `${baseForm}${el['images.directory']}/${el['images.filename']}`, // replace with the actual image URL
+            method: 'GET',
+            responseType: 'stream'
+        });
+
+        const writer = fs.createWriteStream(tmpFilePath);
+        response.data.pipe(writer);
+
+        // Wait for the download to complete
+        writer.on('finish', () => {
+            // Rename the .tmp file to the final file name
+            fs.renameSync(tmpFilePath, filePath);
+            console.log(`Downloaded and saved ${filePath}`);
+            resolve()
+        });
+
+        writer.on('error', (error) => {
+            fs.unlinkSync(tmpFilePath); // Delete the .tmp file on error
+            reject(error.message)
+        });
+
+    } catch (error) {
+        reject(error.message)
+    }
+    })
+}
+fetchImages();
