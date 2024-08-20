0416d2b8c75460429f08988fdbb155e2b44354a1
Slowsby <quentin.peckels@gmail.com>
2024-08-20 16:43:29 +0200

    refactor: eliminate redundancy in if...else statement

--- index.js
+++ index.js
@@ -9,70 +9,46 @@ const API_KEY = process.env.API_KEY;
 const baseForm = "https://eol.jsc.nasa.gov/DatabaseImages/";
 //Consecutive images to download
 const batch = 20;
-
 // Puts CLI args in variables
-let [script, arg, expedition, startRange, endRange] = process.argv.slice(1)
+const [script, arg, expedition, startRange, endRange] = process.argv.slice(1)
+
+const fetchImages = async (url) => {
+    try {
+        let firstIndex = 0;
+        console.log(`Starting fetch request for ${expedition}.`)
+        const response = await fetch(url);
+        const data = await response.json();
+        if (data.result === 'SQL found no records that match the specified criteria') {
+            throw new Error('No records found that match the specified criteria');
+        }
+        console.log(`Successfully fetched data for ${expedition}.`);
+        while (firstIndex < data.length) {
+            const lastIndex = Math.min(firstIndex + batch, data.length);
+            const toDownload = data.slice(firstIndex, lastIndex);
+            
+            // Waits for the completion of the promise before starting new batch
+            await Promise.all(toDownload.map(async (el) => {
+                await downloadImage(el);
+            }));
+            // Update the index for the next batch
+            firstIndex = lastIndex;
+        }
+        console.log('Downloaded.')
+    } catch (error) {
+        console.log(`Error fetching data for ${expedition}`, error.message)
+    }
+}
+
+// Changes the fetch URL based on the command used.
 if (arg === "start") {
     console.log(`The argument provided will trigger the download of the entire database of images of the ${expedition} expedition.`)
     // API DOCS : https://eol.jsc.nasa.gov/SearchPhotos/PhotosDatabaseAPI/#dtaf
     const url = `https://eol.jsc.nasa.gov/SearchPhotos/PhotosDatabaseAPI/PhotosDatabaseAPI.pl?query=images|mission|eq|${expedition}|images|directory|not%20like|*small*|images|directory|not%20like|*lowres*|images|directory|not%20like|*registered*|images|directory|not%20like|*ReefBase*|images|directory|not%20like|*custom*|images|directory|not%20like|*EO*&return=images|directory|images|filename&key=${API_KEY}`;
-    const fetchImages = async () => {
-        try {
-            let firstIndex = 0;
-            console.log(`Starting fetch request for ${expedition}`)
-            const response = await fetch(url);
-            const data = await response.json();
-            if (data.result === 'SQL found no records that match the specified criteria') {
-                throw new Error('No records found that match the specified criteria');
-            }
-            console.log(`Successfully fetched data for ${expedition}`);
-            while (firstIndex < data.length) {
-                const lastIndex = Math.min(firstIndex + batch, data.length);
-                const toDownload = data.slice(firstIndex, lastIndex);
-                
-                // Waits for the completion of the promise before starting new batch
-                await Promise.all(toDownload.map(async (el) => {
-                    await downloadImage(el);
-                }));
-                // Update the index for the next batch
-                firstIndex = lastIndex;
-            }
-            console.log('Downloaded.')
-        } catch (error) {
-            console.log(`Error fetching data for ${expedition}`, error.message)
-        }
-    }
-    fetchImages();
+    fetchImages(url);
 } else if (arg === "range") {
-    console.log(`The argument provided will trigger the download of images numbered from ${parseInt(startRange)} to ${parseInt(endRange)}`);
+    console.log(`The argument provided will trigger the download of images numbered from ${parseInt(startRange)} to ${parseInt(endRange)}.`);
     const url = `https://eol.jsc.nasa.gov/SearchPhotos/PhotosDatabaseAPI/PhotosDatabaseAPI.pl?query=images|mission|eq|${expedition}|images|frame|ge|${parseInt(startRange)}|images|frame|le|${parseInt(endRange)}|images|directory|not%20like|*small*|images|directory|not%20like|*lowres*|images|directory|not%20like|*registered*|images|directory|not%20like|*ReefBase*|images|directory|not%20like|*custom*|images|directory|not%20like|*EO*&return=images|directory|images|filename&key=${API_KEY}`;
-    const fetchImages = async () => {
-        try {
-            let firstIndex = 0;
-            console.log(`Starting fetch request for ${expedition}`)
-            const response = await fetch(url);
-            const data = await response.json();
-            if (data.result === 'SQL found no records that match the specified criteria') {
-                throw new Error('No records found that match the specified criteria');
-            }
-            console.log(`Successfully fetched data for ${expedition}`);
-            while (firstIndex < data.length) {
-                const lastIndex = Math.min(firstIndex + batch, data.length);
-                const toDownload = data.slice(firstIndex, lastIndex);
-                
-                // Waits for the completion of the promise before starting new batch
-                await Promise.all(toDownload.map(async (el) => {
-                    await downloadImage(el);
-                }));
-                // Update the index for the next batch
-                firstIndex = lastIndex;
-            }
-            console.log('Downloaded.')
-        } catch (error) {
-            console.log(`Error fetching data for ${expedition}`, error.message)
-        }
-    }
-    fetchImages();
+    fetchImages(url);
 } else {
     console.log('Invalid argument.')
 }
@@ -82,7 +58,6 @@ const downloadImage = async (el) => {
     try {
         const filePath = path.resolve("GAPE", expedition, el['images.filename']);
         const tmpFilePath = filePath + '.tmp';
-
         // Check if the file already exists
         if (fs.existsSync(filePath)) {
             console.log(`File already exists: ${filePath}`);
